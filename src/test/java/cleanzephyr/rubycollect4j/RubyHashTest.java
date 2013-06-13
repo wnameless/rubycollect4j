6b344a83bd0c10de89c6f2b616ecc39fd34acaf5
WMW <wnameless@gmail.com>
2013-06-14 07:11:29 +0800

    Add test for RubyHash::keys

--- src/test/java/cleanzephyr/rubycollect4j/RubyHashTest.java
+++ src/test/java/cleanzephyr/rubycollect4j/RubyHashTest.java
@@ -285,6 +285,12 @@ public class RubyHashTest {
   }
 
   @Test
+  public void testKeys() {
+    rh = rh(1, 2, 3, 4, 5, 6);
+    assertEquals(ra(1, 3, 5), rh.keys());
+  }
+
+  @Test
   public void testKeyʔ() {
     rh = rh(1, 2, 3, 4, 5, 6);
     assertTrue(rh.keyʔ(1));
